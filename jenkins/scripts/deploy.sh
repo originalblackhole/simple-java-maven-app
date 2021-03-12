8d081255a7fd1cddb413a9dac2fc5663986d9d03
maketozero <makezero@yeah.net>
2021-03-12 22:16:18 +0800

    update Jenkinsfile

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-docker login http://ccr.ccs.tencentyun.com/blackhole/jenkins -u 100018063721 -p gm152688
+sudo docker login --username=100018063721 ccr.ccs.tencentyun.com -p gm152688
+sudo docker tag 5435658a63ac ccr.ccs.tencentyun.com/blackhole/jenkins:3-1-alpine
+sudo docker push ccr.ccs.tencentyun.com/blackhole/jenkins:3-1-alpine
 
-docker push maven:3-alpine
